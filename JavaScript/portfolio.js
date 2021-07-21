860aee4d58ffd0288420959f7fa22e735d295adc
Isaiah <isaiahdj@gmail.com>
2021-07-21 15:54:25 -0700

    Updating Portfolio

--- JavaScript/portfolio.js
+++ JavaScript/portfolio.js
@@ -1,44 +1,44 @@
-// The following function adds a smooth scrolling to all links
-
-$(document).ready(function(){   
-    $("a").on('click', function(event) {
-        
-      // Make sure this.Link has a value before overriding default behavior
-        if (this.Link !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-  
-            // Store Link
-            var Link = this.Link;
-  
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (2000) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(Link).offset().top
-            }, 2000, function(){
-     
-                // Add Link (#) to URL when done scrolling (default click behavior)
-                window.location.Link = Link;
-            });
-        } // End if
-    });
-});
-  
-// These functions open and close the contact form 
+// These functions open and close the contact form
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
-  
+
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
-    this.Close();
 }
 
+// This function displays the first image in the slideshow when the page loads
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// This function changes the slide when the left or right arrows are clicked
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// This function changes the slide when the dots are clicked
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
+    var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
+    if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
+    if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
+    }
+    slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
+    dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
+}
 function openForm_Res() {
     document.getElementById("myForm_Res").style.display = "block";
 }
-
 function closeForm_Res() {
     document.getElementById("myForm_Res").style.display = "none";
-    this.Close();
 }
